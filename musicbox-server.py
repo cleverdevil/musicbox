07a0d51d63ac082051f244daa318ca6347fb228b
Jonathan LaCour <jonathan@cleverdevil.org>
2023-01-13 14:27:20 -0800

    Overhaul of the musicbox server to allow for the playing of both playlists and songs, a more reliable streaming mechanism, and other quality of life improvements.

--- musicbox-server.py
+++ musicbox-server.py
@@ -1,38 +1,91 @@
+from threading import Thread
+
 import os
+import urllib.parse
 import subprocess
+import pathlib
 
 from flask import Flask, jsonify
 
 
 # Configuration
-AIRPLAY_ID = 'E0:2B:96:91:9A:FE'
-
-
-# Synthesize play command
+AIRPLAY_ID = '192.168.7.160'
 directory = os.path.dirname(__file__)
-play_cmd = [f'{directory}/venv/bin/atvremote', '-i', AIRPLAY_ID]
+
+
+def get_playlist_songs(m3u8):
+    '''
+    Parse m3u8 playlist files and return a collection of songs.
+
+    Note: for my use case, I have playlists that refer to m4a versions of
+    the songs, and RAOP works better with MP3, so I change the references.
+    '''
+
+    with open(m3u8) as f:
+        for line in f.readlines():
+            line = urllib.parse.unquote(line.strip()).replace('.m4a', '.mp3')
+            if line and not line.startswith('#'):
+                yield pathlib.Path(directory) / "music" / line
+
+
+class StreamJob(Thread):
+    '''
+    A worker thread which plays a queue of songs in order.
+    Is safely able to be terminated to stop the job.
+    '''
+
+    def __init__(self, queue):
+        self.queue = queue
+        self.current_process = None
+        self.stop = False
+        super().__init__()
+
+    def run(self):
+        for song in self.queue:
+            if not self.stop:
+                self.current_process = self.play(song)
+                self.current_process.wait()
+
+    def play(self, song):
+        print(f'Playing "{song}"')
+        cmd = [f'{directory}/stream', AIRPLAY_ID, song]
+        return subprocess.Popen(cmd, shell=False)
+
+    def terminate(self):
+        self.stop = True
+        if self.current_process:
+            self.current_process.terminate()
 
 
 # Create simple Flask web service
 app = Flask(__name__)
-
 app.now_playing = None
 
 
-@app.route("/<song_id>")
-def play_song(song_id):
+@app.route("/<tag_id>")
+def play(tag_id):
+    # stop existing stream jobs
     if app.now_playing:
         app.now_playing.terminate()
 
-    filename = f'{song_id}.mp3'
-    full_path = f'{directory}/songs/{filename}'
+    # check to see if there is a playlist or song matching the tag
+    path = pathlib.Path(directory) / "music"
+    playlist = path / f'{tag_id}.m3u8'
+    song = path / f'{tag_id}.mp3'
 
-    if not os.path.exists(full_path):
-        print(f'No song found for ID {song_id}')
-        return jsonify({'success': False, 'song_id': song_id})
+    if playlist.exists():
+        print(f'Playing playlist ->', playlist)
+        songs = get_playlist_songs(playlist)
+        app.now_playing = StreamJob(songs)
+        app.now_playing.start()
+        return jsonify({'success': True, 'playing': str(playlist)})
 
-    print(f'Playing song with ID {song_id}')
-    cmd = ' '.join(play_cmd + [f'stream_file="{full_path}"'])
-    app.now_playing = subprocess.Popen(cmd, shell=True)
+    elif song.exists():
+        print(f'Playing song ->', song)
+        app.now_playing = StreamJob([song])
+        app.now_playing.start()
+        return jsonify({'success': True, 'playing': str(song)})
 
-    return jsonify({'success': True, 'playing': filename})
+    print(f'Playlist or song not found for tag id "{tag_id}"')
+    return jsonify({'success': False, 'tag_id': tag_id})
+
